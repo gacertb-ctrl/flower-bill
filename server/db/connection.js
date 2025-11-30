056e4bb627c86bf58a52cb5cedc20e5589b227ba
gacertb-ctrl <gacertb@gmail.com>
2025-11-30 13:58:53 +0530

    Update connection.js

--- server/db/connection.js
+++ server/db/connection.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'admin',
-    password: 'admin',
-    database: 'fm_bill_copy'
+    host: 'mysql-377652b8-gacertb-abf0.g.aivencloud.com',
+    user: 'avnadmin',
+    password: 'AVNS_7DMZCGdPbK8WSsWHgRl',
+    database: 'defaultdb'
 });
 
 module.exports = pool.promise();
